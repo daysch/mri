02b3d29bdc34bab0adb75c230410172fa0689614
Davey Schoenberg <dschoenberg@Anonyputer.local>
2018-06-22 12:19:50 -0400

    fixed tabbing order

--- phantom_create/phantom_app.m
+++ phantom_create/phantom_app.m
@@ -22,7 +22,7 @@ function varargout = phantom_app(varargin)
 
 % Edit the above text to modify the response to help phantom_app
 
-% Last Modified by GUIDE v2.5 22-Jun-2018 12:09:23
+% Last Modified by GUIDE v2.5 22-Jun-2018 12:15:58
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
