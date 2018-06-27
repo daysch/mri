a2b8dd747c0c09fcca40a31816e0a0a365366e38
Davey Schoenberg <dschoenberg@Anonyputer.local>
2018-06-27 12:11:01 -0400

    added do nothing function

---
+++ phantom_create/NOP.m
@@ -0,0 +1,10 @@
+%% https://stackoverflow.com/questions/1904972/how-do-i-make-an-empty-anonymous-function-in-matlab
+function NOP(varargin)
+%NOP Do nothing
+%
+% NOP( ... )
+%
+% A do-nothing function for use as a placeholder when working with callbacks
+% or function handles.
+
+% Intentionally does nothing
