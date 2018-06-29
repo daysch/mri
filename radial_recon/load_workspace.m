5b74cc2fe13e317d2fa1e0db82b40d0572ed78f2
David Schoenberg <davey@jerryb.partners.org>
2018-06-29 15:12:34 -0400

    delete unused save_workspace

--- radial_recon/load_workspace.m
+++
@@ -1,9 +0,0 @@
-% loads the workspace in the current folder into the base matlab workspace
-if exist('workspace', 'file')
-    myvars = load('workspace');
-    for v = fieldnames(myvars)'
-        assignin('base', v{1}, myvars.(v{1}));
-    end
-else
-    msgbox('Image reconstruction is complete, but calculations could not be saved for future use. ','Unable to save workspace data.')
-end
