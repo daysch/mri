a55f52d6821ed2f44652ffce39e2d4915d269ac5
David Schoenberg <davey@jerryb.partners.org>
2018-08-23 14:44:39 -0400

    fix recon default aspect ratio (set to 6.8)

--- radial_recon/display_reconstruction.m
+++ radial_recon/display_reconstruction.m
@@ -1,7 +1,7 @@
 function fig = display_reconstruction(recon_final, recon_matrix_size_val, title)
 % display reconstruction
 addpath([fileparts(fileparts(mfilename('fullpath'))) filesep '3D Viewers' filesep 'vi']); 
-scale = 64/recon_matrix_size_val*8;
+scale = 64/recon_matrix_size_val*6.8;
 fig = vi(abs(recon_final), 'aspect', [scale scale scale]);
 
 % change figure title
